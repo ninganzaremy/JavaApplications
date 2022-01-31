7f733cf27aaa0d39cf0e8b12a26fbe892cb76504
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-31 14:28:11 -0700

    connected Radio Butoons to Listener Object

--- src/com/ninganzaremy/RadioButtons.java
+++ src/com/ninganzaremy/RadioButtons.java
@@ -1,15 +1,19 @@
 package com.ninganzaremy;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
 
 public class RadioButtons {
 
 	private JFrame frame;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
+	JLabel label = new JLabel("");
 
 	/**
 	 * Launch the application.
@@ -25,6 +29,17 @@ public class RadioButtons {
 				}
 			}
 		});
+	}
+	class RadioListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			JRadioButton btn = (JRadioButton)e.getSource();
+			label.setText(btn.getText());
+			
+		}
+		
 	}
 
 	/**
@@ -43,19 +58,29 @@ public class RadioButtons {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Item1");
-		buttonGroup.add(rdbtnNewRadioButton);
-		rdbtnNewRadioButton.setBounds(35, 22, 111, 23);
-		frame.getContentPane().add(rdbtnNewRadioButton);
+		RadioListener listener = new RadioListener()
 		
-		JRadioButton rdbtnItem_1 = new JRadioButton("Item2");
-		buttonGroup.add(rdbtnItem_1);
-		rdbtnItem_1.setBounds(35, 59, 111, 23);
-		frame.getContentPane().add(rdbtnItem_1);
+		JRadioButton item1 = new JRadioButton("Item1");
+		buttonGroup.add(item1);
+		item1.setBounds(35, 22, 111, 23);
+		frame.getContentPane().add(item1);
+		item1.addActionListener(listener);
 		
-		JRadioButton rdbtnItem = new JRadioButton("Item3");
-		buttonGroup.add(rdbtnItem);
-		rdbtnItem.setBounds(35, 100, 111, 23);
-		frame.getContentPane().add(rdbtnItem);
+		JRadioButton item2 = new JRadioButton("Item2");
+		buttonGroup.add(item2);
+		item2.setBounds(35, 59, 111, 23);
+		frame.getContentPane().add(item2);
+		item2.addActionListener(listener);
+
+		
+		JRadioButton item3 = new JRadioButton("Item3");
+		buttonGroup.add(item3);
+		item3.setBounds(35, 100, 111, 23);
+		frame.getContentPane().add(item3);
+		item3.addActionListener(listener);
+
+		
+		label.setBounds(69, 151, 148, 14);
+		frame.getContentPane().add(label);
 	}
 }

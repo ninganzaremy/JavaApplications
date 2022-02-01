c217b475141ada92eec193c050a3704c4b3fc48f
Remy Ninganza <rninganz@asurite.asu.edu>
2022-01-31 18:05:20 -0700

    Publish a runnable JAR file

---
+++ src/com/ninganzaremy/ShareClasses.java
@@ -0,0 +1,19 @@
+package com.ninganzaremy;
+
+public class ShareClasses {
+	public class Person {
+		
+		public String name;
+		
+		public Person(String myName) {
+			name = myName;
+		}
+		
+		public void sayHello() {
+			System.out.println("Hello " + name);
+		}
+		
+	}
+
+
+}
